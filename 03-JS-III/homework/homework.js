022b6efa832c15548d0bfd7e01807103deb07fa8
juan <juan.chocobar@yahoo.com>
2022-11-24 16:11:29 -0300

    JSIII

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,6 +8,11 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    var suma = 0;
+    for (var i = 1; i < 11; i++) {
+      suma = suma + i
+    }
+    return suma
 }
 
 function encuentraPares(array){
@@ -20,17 +25,22 @@ function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  return array.map((elemento) => elemento ** 2);
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+ return array.reduce((anterior, siguiente) => anterior + siguiente);
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  var numeroEnString = num.toString();
+  var longitud = numeroEnString.length;
+  return longitud;
 }
   
   
